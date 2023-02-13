5bd0a6e42fb83af0aa53476f7b72bede64956109
Gehad <jehad.hashish.1999@gmail.com>
2023-02-13 16:43:06 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,44 @@ import { Dialog } from "@reach/dialog";
 import "@reach/dialog/styles.css";
 import { useState } from "react";
 
+function LoginForm({ onSubmit, buttonText }) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { username, password } = event.target.elements;
+    onSubmit({
+      username: username.value,
+      password: password.value,
+    });
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input id="username" name="username" type="text" />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input id="password" name="password" type="password" />
+      </div>
+      <div>
+        <button type="submit">{buttonText}</button>
+      </div>
+    </form>
+  );
+}
+
 function App() {
   const [openModal, setOpenModal] = useState("none");
+  function register(formdata) {
+    console.log("register", formdata);
+  }
+  function login(formdata) {
+    console.log("login", formdata);
+  }
   return (
     <div className="App">
-      <img src={bookLogo} width="80" height="80" />
-      <h1>Book Shelf</h1>
+      <img src={bookLogo} width="100" height="100" />
+      <h1>Bookshelf</h1>
       <div>
         <button
           onClick={() => {
@@ -33,6 +65,7 @@ function App() {
           <button onClick={() => setOpenModal("none")}>x</button>
           <h3>Login</h3>
           <p>This is login form</p>
+          <LoginForm onSubmit={login} buttonText="Login" />
         </Dialog>
         <Dialog
           aria-label="Register form"
@@ -42,6 +75,7 @@ function App() {
           <button onClick={() => setOpenModal("none")}>x</button>
           <h3>Register</h3>
           <p>This is register form</p>
+          <LoginForm onSubmit={register} buttonText="Register" />
         </Dialog>
       </div>
     </div>
